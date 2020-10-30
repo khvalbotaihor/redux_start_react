38277c7d2b40f0c42d8ace8484fc212fe8080907
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-10-30 17:04:51 +0200

    - use redux state in app

--- src/Store/reducer.js
+++ src/Store/reducer.js
@@ -2,12 +2,43 @@ const initialState = {
     counter: 0
 }
 
-const reducer = (state=initialState, action)=> {
-    if(action.type === 'INCREMENT'){
-        return {
-            counter: state.counter +1
-        }
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                counter: state.counter + 1
+            }
+            break;
+        case 'DECREMENT':
+            return {
+                counter: state.counter - 1
+            }
+            break;
+        case 'ADD':
+            return {
+                counter: state.counter + 5
+            }
+            break;
+        case 'SUBSTRACT':
+            return {
+                counter: state.counter - 5
+            }
+            break;
+        default:
+            return state
+            break;
+
     }
+
+
+    /*
+        if(action.type === 'INCREMENT'){
+            return {
+                counter: state.counter +1
+            }
+        }
+    */
+
 
     return state;
 }
