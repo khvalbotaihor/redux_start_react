a60a540ef9eda8f31b1a77067f9e9cbbfbce14aa
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-10-30 16:23:48 +0200

    - added reducer to the store

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import {createStore} from 'redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import reducer from './Store/reducer';
 
-const store=createStore();
+
+const store=createStore(reducer);
 
 ReactDOM.render(
     <App />
